c5e6125877a87b9e5fd5ed7ce73987eab237f6d9
Etienne Vautherin <etienne@vautherin.com>
2025-02-19 15:43:19 +0100

    Initial Commit

---
+++ Myeline/MyelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyelineApp.swift
+//  Myeline
+//
+//  Created by Etienne Vautherin on 19/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
